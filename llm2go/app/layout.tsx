12409c0fb5eb9050011544a88c76afd363d4d3a8
cyborgoat <cyborgoat@outlook.com>
2025-01-25 14:45:46 +0800

    init project

---
+++ llm2go/app/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>React App</title>
+        <meta name="description" content="Web site created..." />
+      </head>
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  )
+}
